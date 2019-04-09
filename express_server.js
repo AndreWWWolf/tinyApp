9e71c467a2af7c81c875a08d2466e870a676155e
Andrew Wolf <andrew.wolfeus@gmail.com>
2019-04-09 20:10:56 +0000

    views modified and .get function modified

--- express_server.js
+++ express_server.js
@@ -17,8 +17,13 @@ var urlDatabase = {
 //   res.json(urlDatabase);
 // });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
