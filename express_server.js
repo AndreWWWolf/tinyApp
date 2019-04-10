998e14e0518228f52ccc45c06fb9368d5f214744
Andrew Wolf <andrew.wolfeus@gmail.com>
2019-04-10 18:25:28 +0000

    updated files to cleaner code

--- express_server.js
+++ express_server.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
 // });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({extended: true}));
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -50,3 +43,4 @@ function generateRandomString () {
   let randomString = Math.random().toString(36).slice(-6);
     return randomString;
 }
+generateRandomString();
