74c3f7f021d504dc10556757587733d56ceffbda
Andrew Wolf <andrew.wolfeus@gmail.com>
2019-04-11 21:48:55 +0000

    url shortening 2 completed fully

--- express_server.js
+++ express_server.js
@@ -12,29 +12,46 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+// displays home page with short and longURLS
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase };
+    console.log("i am two");
+  res.render("urls_index", templateVars);
+});
+// create new login URL page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+  console.log("i am five");
 });
-
+// creates new custom shortURL for given longURL
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let shortURL = generateRandomString();
+  console.log(shortURL);
+  console.log(req.body.longURL)
+  urlDatabase[shortURL] = req.body.longURL
+  longURL = req.body.longURL
+  console.log("i am four")
+  res.redirect(`/urls/${shortURL}`);
+});
+// will read anything in browser urls/""/ and make a href link out of it
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL] };
+    console.log("i am one");
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = {
-    urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+// redirects shortURL links to longURL page
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = req.body.longURL;
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log("i am three");
+  console.log(longURL);
   res.redirect(longURL);
 });
 
@@ -42,14 +59,6 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  let shortURL = generateRandomString();
-  console.log(shortURL);
-  console.log(req.body.longURL)
-  urlDatabase[shortURL] = req.body.longURL
-  res.redirect(`/urls/${shortURL}`);
-});
 
 function generateRandomString () {
   let randomString = Math.random().toString(36).slice(-6);
